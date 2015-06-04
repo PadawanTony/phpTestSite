d179d15fc9a2f4b274064b4def1d959c2774530a
Antony <anthonykalogeropoulos@gmail.com>
2015-06-04 23:51:41 +0300

    Test Monolog - Create app.txt

--- index.php
+++ index.php
@@ -12,10 +12,9 @@ date_default_timezone_set('Europe/Athens');
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 
-$log = new Logger('name');  //Because of the use statement, Logger is equal to Monolog\Logger
-$log->pushHandler(new StreamHandler('app.txt', Logger::WARNING));
-
-$log->addWarning('Oh No!');
+// $log = new Logger('name');  //Because of the use statement, Logger is equal to Monolog\Logger
+// $log->pushHandler(new StreamHandler('app.txt', Logger::WARNING));
+// $log->addWarning('Oh No!');
 
 echo "Hello World";
 
