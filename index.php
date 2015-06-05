389aa5316f4ebd5706bb2afb6a297adb23b9c53e
Antony <anthonykalogeropoulos@gmail.com>
2015-06-05 18:15:03 +0300

    Hello world example using Slim and .htaccess

--- index.php
+++ index.php
@@ -12,10 +12,16 @@ date_default_timezone_set('Europe/Athens');
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 
+$app = new \Slim\Slim();
+
 // $log = new Logger('name');  //Because of the use statement, Logger is equal to Monolog\Logger
 // $log->pushHandler(new StreamHandler('app.txt', Logger::WARNING));
 // $log->addWarning('Oh No!');
 
-echo "Hello World";
+$app->get('/hello/:name', function ($name) {
+	echo "Hello, $name";
+});
+
+$app->run();
 
 ?>
