5896e23033b29a5839c47406dd92698dea442f57
Antony <anthonykalogeropoulos@gmail.com>
2015-06-13 21:04:11 +0300

    Set up SwiftMailer

--- index.php
+++ index.php
@@ -33,11 +33,44 @@ $view->parserExtensions = array(
 
 $app->get('/', function() use($app) {
 	$app->render('about.twig');
-});
+})->name('about');
+
 $app->get('/contact', function() use($app) {
 	$app->render('contact.twig');
-});
+})->name('contact');
 
+$app->post('/contact', function() use($app) {
+	$name = $app->request->post('name');
+	$email = $app->request->post('email');
+	$msg = $app->request->post('msg');
+
+	if( !empty($name) && !empty($email) && !empty($msg) ) {
+		$cleanName = filter_var($name, FILTER_SANITIZE_STRING);
+		$cleanEmail = filter_var($name, FILTER_SANITIZE_EMAIL);
+		$cleanMsg = filter_var($name, FILTER_SANITIZE_STRING);
+	} else {
+		$app->redirect('/contact');
+	}
+
+	$transport = Swift_MailTransport::newInstance();
+	$mailer = Swift_Mailer::newInstance($transport);
+
+	$message = Swift_Message::newInstance();
+	$message->setSubject("Email from our Website");
+	$message->setFrom(array(
+		$cleanEmail => $cleanName
+	));
+	$message->setTo(array('anthonykalogeropoulos@gmail.com' => "Anthony Kalos"));
+	$message->setBody($cleanMsg);
+
+	$result = $mailer->send($message);
+
+	if ($result > 0) {
+		$app->redirect('/');
+	} else {
+		$app->redirect('/contact');
+	}
+});
 
 $app->run();
 
